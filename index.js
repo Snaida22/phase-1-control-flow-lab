8f873d944ace08b6b790a268ec5b395736e4308e
Snaida.Kabalika <snaida.kabalika@student.moringaschool.com>
2022-06-06 22:38:58 +0300

    commit tree 3a309c63586430388e983f9df37e43b544179944

--- index.js
+++ index.js
@@ -1,11 +1,37 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(freeSam){
   // Write your code here!
+  if(freeSam == 199){
+    return "This one is on me!"
+  }
+  else if (freeSam> 2000 && freeSam < 2500){
+    return "I will gladly take your thirty bucks."
+  }
+  else if (freeSam > 2500){
+    return "No can do."
+  }
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(snyCity){
   // Write your code here!
+  if (snyCity == "NYC"){
+    return "Ok, sounds good."
+  }
+  else{
+    return "No go.";
+  }
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(driverTip){
   // Write your code here!
+  switch(driverTip){
+    case 'generous':
+      return "Thank you so much."
+      break;
+    case 'not as generous':
+    return "Thank you."
+    break;
+    default:
+      return "Bye."
+  
+  }
 }
